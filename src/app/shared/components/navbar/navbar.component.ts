e9ca416b2a07f1affd034b7f8a8cdbebbc00e769
Joshy14Y <joshygamer14y@gmail.com>
2025-02-24 03:26:32 -0600

    navbar-component

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'shared-navbar',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent {}
